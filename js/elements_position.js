6c96c1ec841cbda2cac427b59de0bf9c92007f3a
Andrew Garyga <andrewgaryga@gmail.com>
2016-08-09 15:40:03 +0300

    done

--- js/elements_position.js
+++ js/elements_position.js
@@ -1,22 +1,12 @@
-// $(window).resize(function(){
-//         // alert("Height of div: " + $("div").height());
-//         // console.log ($(".benefits").height());
-//         // x = $(".benefits").height() * 0.14;
-//         // console.log (x);
-//         // $(".btn-success").css("margin-top", x);
-//         x = $(".benefits").height();
-//         console.log (x);
-//         $(".no-title").css("height", x);
-// });
-
-$(window).on("resize", function () {
+jQuery(document).ready(function ($) {
+$(window).on('resize', function() {
     if ( $(window).width() > 992) { 
-	    x = $(".benefits").height();
-	    x = x + 1;
+	    x = $(".benefits").outerHeight();
 	    console.log (x);
 	    $(".no-title").css("height", x);
     }
     else {
   		$(".no-title").css("height", "auto");
 	}
-}).resize();
+}).trigger('resize');
+});
