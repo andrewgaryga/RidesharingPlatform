15b3d46072ba5856830088570b72a2b0ade34673
Andrew Garyga <andrewgaryga@gmail.com>
2016-08-17 12:18:32 +0300

    done

--- js/elements_position.js
+++ js/elements_position.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
 $(window).on('resize', function() {
     if ( $(window).width() > 992) { 
 	    x = $(".benefits").outerHeight();
-	    console.log (x);
+	    // console.log (x);
 	    $(".no-title").css("height", x);
     }
     else {
